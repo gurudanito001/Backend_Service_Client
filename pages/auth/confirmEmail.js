7cdeb8ca217a57ebb96c9aea500cc01d29229407
Daniel Nwokocha <gurudanito001@gmail.com>
2023-01-10 17:14:39 +0100

    added coming soon to two pages

---
+++ pages/auth/confirmEmail.js
@@ -0,0 +1,12 @@
+
+
+
+const confirmEmail = () =>{
+  return (
+    <div className="d-flex align-items-center justify-content-center" style={{height: "100vh"}}>
+      <h2 className="border">Coming Soon . . .</h2>
+    </div>
+  )
+}
+
+export default confirmEmail;
