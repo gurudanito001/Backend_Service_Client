c938173fdb40d2d79ceda0b17905f826aa8a4216
Daniel Nwokocha <gurudanito001@gmail.com>
2023-01-28 15:43:44 +0100

    test commit

--- pages/static/navbar.js
+++ pages/static/navbar.js
@@ -3,13 +3,13 @@ import React from "react";
 const navbar = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a class="navbar-brand" href="#">
+          <a className="navbar-brand" href="#">
             Marlayer
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNavAltMarkup"
@@ -17,17 +17,17 @@ const navbar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav ms-auto">
-            <a class="nav-item nav-link active p-3" href="#">
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav ms-auto">
+            <a className="nav-item nav-link active p-3" href="#">
                 docs
               </a>
-              <a class="nav-item nav-link active" href="#">
+              <a className="nav-item nav-link active" href="#">
                 <button className="btn btn-primary">Get Started</button>
               </a>
-              <a class="nav-item nav-link active" href="#">
+              <a className="nav-item nav-link active" href="#">
                 <button className="btn btn-success">Login</button>
               </a>
             </div>
